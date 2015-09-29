71fcad061ed53a1aaf6cfc492efe3b6bea9207c3
luke <luke.belliveau@gmail.com>
2015-09-29 16:00:33 -0400

    POST request

--- app.js
+++ app.js
@@ -10,6 +10,26 @@ app.get('/', function(req, res) {
     res.send("Welcome to Weather-Monitoring Server!");
 });
 
+request({
+        url: 'https://482ce492-5318-4aa3-86fd-27cabcf1aac0-bluemix.cloudant.com/test/_all_docs?limit=100',
+        method: 'POST',
+        
+        form: {
+            "_id": "apple",
+            "item": "Malus domestica",
+            "prices": {
+                "Fresh Mart": 1.59,
+                "Price Max": 5.99,
+                "Apples Express": 0.79
+            }
+        }
+}, function(error, response, body){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(response.statusCode, body);
+        }
+});
 
 
 
