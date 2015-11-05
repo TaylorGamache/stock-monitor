22d153c3cdfd7c4aa350e976e8f1d050c6aa69d0
luke <luke.belliveau@gmail.com>
2015-11-05 13:42:49 -0500

    temperature watcher demo is completed. need to implement POST recipe handler to complete demo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var Cloudant = require('cloudant');
 var json = require('json');
 var me = 'lukebelliveau';
 var password = 'weathermonitor';
-var weatherAPIKey = "02866fb0b0b72a03f9"
+var weatherAPIKey = "02866fb0b72a03f9"
 
 var app = express();
 
@@ -30,49 +30,61 @@ app.get('/', function(req, res){
 
 app.get('/test', function(req, res){
 	res.send("test page");
-	watchForTemperature();
-})
-
-app.get('/hello', function(req, res){
-	res.send("you're at hello");
-
-	var job = {};
-	job['hello'] = "hi";
-	job['no'] = 'yes';
-	console.log(job);
+	watchForTemperatureHelper(20, "LT", "http://nsds-api-stage.mybluemix.net/api/v1/trigger/", "dummyID", "Storrs", "CT", 1000);
+	// watchForTemperature(200, "LT", "http://nsds-api-stage.mybluemix.net/api/v1/trigger/", "dummyID", "Storrs", "CT");
 })
 
 app.post('/recipes', function(req, res){
+	/**
+	1.) Store recipe in DB and return response
+	2.) start watchForTemperatureHelper
+	*/
+
 	var request = req.body;
 	console.log(request.recipe);
 	
 })
 
-function watchForTemperature(temp, relation, callback, recipeID){
-	// if(compare != "<"){
-	// 	//this will be extended to also do equal to, greater than
-	// 	console.log("invalid comparison signal");
-	// 	return;
-	// }
+function watchForTemperatureHelper(targetTemp, relation, callback, recipeID, city, state, timeout){
+	setInterval(watchForTemperature(targetTemp, relation, callback, recipeID, city, state), timeout);
+}
 
-	requestURL = "http://api.wunderground.com/api/02866fb0b72a03f9/conditions/q/CA/San_Francisco.json";
-	var temp;
+function watchForTemperature(targetTemp, relation, callback, recipeID, city, state){
+	if(relation != "LT"){
+		//this will be extended to also do equal to, greater than
+		console.log("invalid comparison signal");
+		return;
+	}
+
+	requestURL = "http://api.wunderground.com/api/"
+	requestURL += weatherAPIKey + "/conditions/q/"
+	requestURL += state + "/" + city + ".json";
+
 	request(requestURL, function(err, response, body){
 		if(!err){
 			var parsedbody = JSON.parse(body);
-		temp = parsedbody.current_observation.temp_f;
+			var currentTemp = parsedbody.current_observation.temp_f;
+			console.log("current temp: " + currentTemp);
+			if(currentTemp < targetTemp){
+				console.log("Target hit, calling callback URL...");
+				callback += recipeID;
+				request(callback, function(err, response, body){
+					if(!err){
+						console.log("successfully sent trigger, response body:");
+						console.log(body);
+					}else{
+						console.log(response);
+						throw err;
+					}
+				});
+			}
 		}else{
 			console.log(response);
 			throw err;
 		}
-
-	console.log("temp: " + temp);
 		
 	});
-
 }
-
-
 
 app.listen(port);
 
